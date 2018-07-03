78350a2e5bfed6b93eb9e3c1a5e93dacb007485b
herton7362 <he.tang@everydayratings.com>
2018-07-03 21:38:39 +0800

    fix bug

--- src/main/java/com/herton/module/orderform/web/PurchaseOrderController.java
+++ src/main/java/com/herton/module/orderform/web/PurchaseOrderController.java
@@ -1,7 +1,9 @@
 package com.herton.module.orderform.web;
 
+import com.herton.common.AbstractCrudController;
 import com.herton.common.PageParam;
 import com.herton.common.PageResult;
+import com.herton.module.orderform.domain.PurchaseOrder;
 import com.herton.module.orderform.dto.PurchaseOrderDTO;
 import com.herton.module.orderform.service.PurchaseOrderService;
 import io.swagger.annotations.Api;
@@ -20,42 +22,5 @@ import java.util.Map;
 @Api(value = "采购订单单管理")
 @RestController
 @RequestMapping("/api/purchaseOrder")
-public class PurchaseOrderController {
-    private final PurchaseOrderService purchaseOrderService;
-
-    /**
-     * 保存
-     */
-    @ApiOperation(value="保存")
-    @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity<?> save(@RequestBody PurchaseOrderDTO purchaseOrderDTO) throws Exception {
-        purchaseOrderService.save(purchaseOrderDTO);
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    /**
-     * 查询
-     */
-    @ApiOperation(value="分页如果不传页数则按照条件查询", notes = "可以传查询条件例：name=张三，查询条件使用or分割")
-    @ApiImplicitParams(value = {
-            @ApiImplicitParam(name = "currentPage", value = "当前页数", dataType = "Integer", paramType = "query"),
-            @ApiImplicitParam(name = "pageSize", value = "每页页数", dataType = "Integer", paramType = "query"),
-            @ApiImplicitParam(name = "sort", value = "排序属性，多个用逗号隔开", dataType = "String", paramType = "query"),
-            @ApiImplicitParam(name = "order", value = "排序方向，多个用逗号隔开", dataType = "String", paramType = "query")
-    })
-    @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<?> searchPagedList(@ModelAttribute PageParam pageParam, HttpServletRequest request) throws Exception {
-        Map<String, String[]> param = request.getParameterMap();
-        if(pageParam.isPageAble()) {
-            PageResult<PurchaseOrderDTO> page = purchaseOrderService.findAllTranslated(pageParam.getPageRequest(), param);
-            return new ResponseEntity<>(page, HttpStatus.OK);
-        }
-        List<PurchaseOrderDTO> list = purchaseOrderService.findAllTranslated(param);
-        return new ResponseEntity<>(list, HttpStatus.OK);
-    }
-
-    @Autowired
-    public PurchaseOrderController(PurchaseOrderService purchaseOrderService) {
-        this.purchaseOrderService = purchaseOrderService;
-    }
+public class PurchaseOrderController extends AbstractCrudController<PurchaseOrder, PurchaseOrderDTO> {
 }
