89ef97dd12ba8ae81adebc2bb4a0f6eacf8ec9af
herton7362 <he.tang@everydayratings.com>
2018-07-19 17:07:50 +0800

    修改异常基类

--- src/main/java/com/herton/exceptions/BusinessException.java
+++ src/main/java/com/herton/exceptions/BusinessException.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 
 import java.io.Serializable;
 
-abstract class BusinessException extends Exception implements Serializable {
+abstract class BusinessException extends RuntimeException implements Serializable {
     private static final long serialVersionUID = 6807228021289834441L;
     private static final Logger LOG = LoggerFactory.getLogger(BusinessException.class);
     private BusinessException() {}
